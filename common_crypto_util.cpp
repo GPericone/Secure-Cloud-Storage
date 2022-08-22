2423ec1e2f75d7cc7a156d1e79898443c221da87
GPericone <46968685+GPericone@users.noreply.github.com>
2022-08-22 17:18:38 +0200

    Create common_crypto_util.cpp

---
+++ common_crypto_util.cpp
@@ -0,0 +1,17 @@
+#include <openssl/evp.h>
+
+
+/*
+
+Funzioni di OPENSSL da utilizzare:
+
+- Creazione del certificato
+- Protocollo di scambio delle chiavi DH
+- Digital Envelope
+- Protocollo di autenticazione
+
+*/
+
+// --------------------------------------------------------------------------
+// DH
+// --------------------------------------------------------------------------
