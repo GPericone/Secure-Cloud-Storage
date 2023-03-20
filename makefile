ce321ec230dd5a5f8074215cf091ba1cec2d88e2
Vito <vito.chiumiento@libero.it>
2023-03-20 22:51:39 +0100

    fix varie

--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 all: remove client server
 
 remove: 
-# 	rm client
-	rm server
+	# rm client
+	# rm server
 
-client: msg_client.cpp client_util.cpp
-	g++ -g -Wall -o client msg_client.cpp client_util.cpp common_util.cpp -lcrypto
+client: client.cpp tmp_handshake.cpp utils.cpp
+	g++ -g -Wall -o client client.cpp tmp_handshake.cpp utils.cpp -lcrypto
 
-server: testmain.cpp file_server_util.cpp
-	g++ -g -Wall -o server testmain.cpp file_server_util.cpp
+server: server.cpp tmp_handshake.cpp utils.cpp
+	g++ -g -Wall -o server server.cpp tmp_handshake.cpp utils.cpp -lcrypto
--- tmp_handshake.cpp
+++ tmp_handshake.cpp
@@ -7,7 +7,7 @@ int send_message1(int socket)
     std::cin >> username;
 
     // Definire la costante USERNAMESIZE nell'header
-    if (username.empty() || username.size() > USERNAMESIZE) 
+    if (username.empty() || username.size() > USERNAMESIZE)
     {
         std::cout << "Invalid username" << std::endl;
         return -1;
@@ -15,17 +15,18 @@ int send_message1(int socket)
 
     // Generate nonce with OpenSSL
     unsigned char nonce[NONCE_LEN];
-    if (RAND_bytes(nonce, NONCE_LEN) != 1) {
+    if (RAND_bytes(nonce, NONCE_LEN) != 1)
+    {
         std::cout << "Error generating nonce" << std::endl;
         return -1;
     }
 
     // Calculate payload size
-    unsigned char* payload_size_byte, *message;
+    unsigned char *payload_size_byte, *message;
     size_t payload_size = username.size() + NONCE_LEN;
 
     // Create message buffer
-    size_t message_size = sizeof(int) + payload_size;
+    // size_t message_size = sizeof(int) + payload_size;
     memory_handler(1, socket, payload_size, &payload_size_byte);
     serialize_int(payload_size, payload_size_byte);
 
@@ -38,15 +39,16 @@ int send_message1(int socket)
     memory_handler(1, socket, message_size, &message);
 
     // Copy nonce and username into message buffer
-    
+
     memcpy(message, payload_size_byte, sizeof(int));
-	memcpy((unsigned char*)&message[sizeof(int)], nonce, NONCE_LEN);
-	memcpy((unsigned char*)&message[sizeof(int) + NONCE_LEN], username.c_str(), username.size());
+    memcpy((unsigned char *)&message[sizeof(int)], nonce, NONCE_LEN);
+    memcpy((unsigned char *)&message[sizeof(int) + NONCE_LEN], username.c_str(), username.size());
 
     // Send message
     int bytes_sent = send(socket, message, message_size, 0);
 
-    if (bytes_sent < 0) {
+    if (bytes_sent < 0)
+    {
         free_var(1);
         std::cout << "Error sending message" << std::endl;
         return -1;
@@ -55,20 +57,20 @@ int send_message1(int socket)
     return 0;
 }
 
-// int send_message2(int socket, std::string username, X509* certificate, EVP_PKEY* server_private_key, unsigned char nonceC[]) 
+// int send_message2(int socket, std::string username, X509* certificate, EVP_PKEY* server_private_key, unsigned char nonceC[])
 // {
 //     // 1. Load client public key from .pem file
 //     std::string client_public_key_file_str = "clients/keys/publickey" + username + ".pem";
 //     const char* client_public_key_file = client_public_key_file_str.c_str();
 //     EVP_PKEY* client_public_key = load_public_key(client_public_key_file);
-//     if (!client_public_key) 
+//     if (!client_public_key)
 //     {
 //         return -1;
 //     }
 
 //     // 2. Generate nonceS
 //     unsigned char nonceS[NONCE_LEN];
-//     if (RAND_bytes(nonceS, NONCE_LEN) != 1) 
+//     if (RAND_bytes(nonceS, NONCE_LEN) != 1)
 //     {
 //         std::cerr << "Error generating nonceS" << std::endl;
 //         EVP_PKEY_free(client_public_key);
@@ -77,7 +79,7 @@ int send_message1(int socket)
 
 //     // 3. Create AES-256 session key
 //     unsigned char session_key[32];
-//     if (RAND_bytes(session_key, sizeof(session_key)) != 1) 
+//     if (RAND_bytes(session_key, sizeof(session_key)) != 1)
 //     {
 //         std::cerr << "Error generating session key" << std::endl;
 //         EVP_PKEY_free(client_public_key);
@@ -99,7 +101,7 @@ int send_message1(int socket)
 //     encrypted_envelope_len = envelope_encrypt(client_public_key, plaintext, sizeof(plaintext), sym_key_enc, sym_key_len, iv, encrypted_envelope);
 //     EVP_PKEY_free(client_public_key);
 
-//     if (encrypted_envelope_len < 0) 
+//     if (encrypted_envelope_len < 0)
 //     {
 //         return -1;
 //     }
@@ -107,7 +109,7 @@ int send_message1(int socket)
 //     // 6. Serialize the certificate
 //     unsigned char* cert_buf;
 //     int cert_len = i2d_X509(certificate, &cert_buf);
-//     if (cert_len < 0) 
+//     if (cert_len < 0)
 //     {
 //         std::cerr << "Error serializing certificate" << std::endl;
 //         return -1;
@@ -136,7 +138,7 @@ int send_message1(int socket)
 
 //     // Send message
 //     int bytes_sent = send(socket, message, message_size, 0);
-//     if (bytes_sent < 0) 
+//     if (bytes_sent < 0)
 //     {
 //         std::cerr << "Error sending message" << std::endl;
 //     }
@@ -147,10 +149,9 @@ int send_message1(int socket)
 //     return bytes_sent;
 // }
 
-
 // int send_message3(int socket, unsigned char* nonceS, EVP_PKEY* client_private_key)
 // {
-    
+
 //     int ret;
 
 //     // Encrypt the nonceS with the client's private key
